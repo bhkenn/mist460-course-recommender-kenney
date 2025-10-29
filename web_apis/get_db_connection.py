2e26e97e934a05e8caf4b9579183855f7652e3cd
Braxton Kenney <146278620+bhkenn@users.noreply.github.com>
2025-10-29 18:12:41 -0400

    10/29

--- web_apis/get_db_connection.py
+++ web_apis/get_db_connection.py
@@ -1,18 +1,91 @@
 from fastapi import FastAPI, HTTPException
-import pyodbc
 
-#DB_SERVER = 'mist-kenney.database.windows.net'
-#DB_DATABASE = 'MIST460_RelationalDatabase_Kenney'
-#DB_DRIVER = '{ODBC Driver 17 for SQL Server}'
+import pyodbc #Python Open DataBase Connectivity library
 
-DB_SERVER = 'localhost\\BRAXTONS-LAPTOP'
-DB_DATABASE = 'MIST460_RelationalDatabase_Lastname'
-DB_DRIVER = '{ODBC Driver 17 for SQL Server}'
+import os
+
+from dotenv import load_dotenv
+
+from pathlib import Path
+
+# Load environment variables from .env file
+
+path = Path(__file__).parent.parent / '.env'
+
+load_dotenv(dotenv_path=path)
+
+
 
 def get_db_connection():
-    try:
-        conn_str = f'DRIVER={DB_DRIVER};SERVER={DB_SERVER};DATABASE={DB_DATABASE};trusted_connection=yes;'
-        return pyodbc.connect(conn_str)
-    except Exception as e:
-        print(f"Error connecting to database: {e}")
-        raise HTTPException(status_code=500, detail="Database connection error")
+
+
+  environment = os.getenv('ENVIRONMENT')
+
+  if environment == 'PRODUCTION':
+
+    DB_SERVER = os.getenv('DB_SERVER')
+
+    DB_DATABASE = os.getenv('DB_NAME')
+
+    DB_USERNAME = os.getenv('DB_USER')
+
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+    DB_DRIVER = os.getenv('DB_DRIVER')
+
+    connection_string = (   
+
+      f'DRIVER={DB_DRIVER};'
+
+      f'SERVER={DB_SERVER};'
+
+      f'DATABASE={DB_DATABASE};'
+
+      f'UID={DB_USERNAME};'
+
+      f'PWD={DB_PASSWORD};'
+
+      'Encrypt=yes;'
+
+      'TrustServerCertificate=no;'
+
+      'Connection Timeout=30;'
+
+    )
+
+  else:
+
+    #Development environment settings
+
+    DB_SERVER = 'localhost\\BRAXTONS-LAPTOP'
+
+    DB_DATABASE = 'MIST460_RelationalDatabase_Lastname'
+
+    DB_DRIVER = '{ODBC Driver 18 for SQL Server}'
+
+    connection_string = (   
+
+      f'DRIVER={DB_DRIVER};'    
+
+      f'SERVER={DB_SERVER};'
+
+      f'DATABASE={DB_DATABASE};'
+
+      'Encrypt=yes;'
+
+      'Trusted_Connection=yes;'
+
+      'Connection Timeout=30;'
+
+      'TrustServerCertificate=yes;'
+
+    )
+
+
+  try:
+
+    return pyodbc.connect(connection_string)
+
+  except Exception as e:
+
+    raise HTTPException(status_code=500, detail="Database connection error")
